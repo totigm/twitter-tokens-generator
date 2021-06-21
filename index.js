9c29cc280926a5525bead2a5be08976acd10ebce
Toti Muñoz <gonzalo.munoz@southworks.com>
2021-06-21 18:36:22 -0300

    🎨 Improve code structure

--- index.js
+++ index.js
@@ -1,59 +1,27 @@
 const express = require("express");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
 const LoginWithTwitter = require("login-with-twitter");
-const dotenv = require("dotenv");
 const open = require("open");
-fs = require("fs");
+const clipboardy = require("clipboardy");
+require("dotenv").config();
 
-dotenv.config();
+const { env } = process;
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = env.PORT ?? 3000;
 
-app.use(cookieParser());
-app.use(
-    session({
-        secret: "keyboard cat",
-    })
-);
+const credentials = {
+    consumerKey: env.CONSUMER_KEY,
+    consumerSecret: env.CONSUMER_SECRET,
+    callbackUrl: "http://localhost:3000/twitter/callback",
+};
 
-const tws = [];
+const twitter = new LoginWithTwitter(credentials);
 
-app.get("/twitter", (req, res) => {
-    const credentials = {
-        consumerKey: req.query.consumerKey,
-        consumerSecret: req.query.consumerSecret,
-        callbackUrl:
-            req.query.callbackUrl ?? "http://localhost:3000/twitter/callback",
-    };
+let token;
 
-    const tw = new LoginWithTwitter(credentials);
-
-    tw.login((err, tokenSecret, url) => {
-        if (err) {
-            // Handle the error your way
-        }
-
-        // Save the OAuth token secret for use in your /twitter/callback route
-        req.session.twitter = {
-            credentials,
-            tokenSecret,
-            saveAsDotEnv: req.query.saveAsDotEnv ?? false,
-        };
-
-        req.session.tokenSecret = tokenSecret;
-        req.session.saveAsDotEnv = req.query.saveAsDotEnv ?? false;
-        req.session.credentials = credentials;
-
-        tws.push({
-            token: tokenSecret,
-            tw,
-        });
-
-        // Redirect to the /twitter/callback route, with the OAuth responses as query params
-        res.redirect(url);
-    });
+twitter.login((err, tokenSecret, url) => {
+    token = tokenSecret;
+    open(url);
 });
 
 app.get("/twitter/callback", (req, res) => {
@@ -62,41 +30,28 @@ app.get("/twitter/callback", (req, res) => {
         oauth_verifier: req.query.oauth_verifier,
     };
 
-    const token = req.session.tokenSecret;
-    delete req.session.tokenSecret;
-    const twIndex = tws.findIndex((tw) => tw.token === token);
+    twitter.callback(oAuthParam, token, (err, user) => {
+        if (!user) res.status(404).send("User not found :c");
 
-    tws[twIndex].tw.callback(oAuthParam, token, (err, user) => {
-        req.session.user = user;
+        const twitterCredentials = {
+            CONSUMER_KEY: credentials.consumerKey,
+            CONSUMER_SECRET: credentials.consumerSecret,
+            ACCESS_TOKEN: user.userToken,
+            SECRET_ACCESS_TOKEN: user.userTokenSecret,
+        };
 
-        tws.splice(twIndex, 1);
-        if (!user) res.send("rip");
-        else {
-            const credentials = req.session.credentials;
-            delete req.session.credentials;
+        let credentialsString = "";
+        Object.entries(twitterCredentials).forEach(([key, value]) => {
+            credentialsString += `${key}=${value}\n`;
+        });
 
-            const generatedCredentials = {
-                CONSUMER_KEY: credentials.consumerKey,
-                CONSUMER_SECRET: credentials.consumerSecret,
-                ACCESS_TOKEN: user.userToken,
-                SECRET_ACCESS_TOKEN: user.userTokenSecret,
-            };
-
-            // Redirect to whatever route that can handle your new Twitter login user details!
-            res.send(generatedCredentials);
-            if (req.session.saveAsDotEnv === "true") {
-                let dotenvFile = "";
-                Object.entries(generatedCredentials).forEach(([key, value]) => {
-                    dotenvFile += `${key}=${value}\n`;
-                });
-
-                fs.writeFile(".env", dotenvFile, function (err) {
-                    if (err) return console.log(err);
-                });
-
-                delete req.session.saveAsDotEnv;
-            }
-        }
+        clipboardy.writeSync(credentialsString);
+        console.log("Your credentials have been copied to your clipboard!");
+        res.status(200).send(
+            `${JSON.stringify(
+                twitterCredentials
+            )}<script>alert("Your credentials have been copied to your clipboard! You can close this now.");</script>`
+        );
     });
 });
 
