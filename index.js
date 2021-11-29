da8c9f0548dc93782ef9bd9a2a4a84a8ba3a2cf8
Toti Muñoz <totigm@gmail.com>
2021-11-29 20:38:39 -0300

    :art: Format code

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const app = express();
 const port = env.PORT ?? 3000;
 
 const credentials = {
-    consumerKey: env.CONSUMER_KEY,
-    consumerSecret: env.CONSUMER_SECRET,
-    callbackUrl: "http://localhost:3000/twitter/callback",
+  consumerKey: env.CONSUMER_KEY,
+  consumerSecret: env.CONSUMER_SECRET,
+  callbackUrl: "http://localhost:3000/twitter/callback",
 };
 
 const twitter = new LoginWithTwitter(credentials);
@@ -20,41 +20,43 @@ const twitter = new LoginWithTwitter(credentials);
 let token;
 
 twitter.login((err, tokenSecret, url) => {
-    token = tokenSecret;
-    open(url);
+  token = tokenSecret;
+  open(url);
 });
 
 app.get("/twitter/callback", (req, res) => {
-    const oAuthParam = {
-        oauth_token: req.query.oauth_token,
-        oauth_verifier: req.query.oauth_verifier,
+  const oAuthParam = {
+    oauth_token: req.query.oauth_token,
+    oauth_verifier: req.query.oauth_verifier,
+  };
+
+  twitter.callback(oAuthParam, token, (err, user) => {
+    if (!user) res.status(404).send("User not found :c");
+
+    const twitterCredentials = {
+      CONSUMER_KEY: credentials.consumerKey,
+      CONSUMER_SECRET: credentials.consumerSecret,
+      ACCESS_TOKEN: user.userToken,
+      ACCESS_TOKEN_SECRET: user.userTokenSecret,
     };
 
-    twitter.callback(oAuthParam, token, (err, user) => {
-        if (!user) res.status(404).send("User not found :c");
-
-        const twitterCredentials = {
-            CONSUMER_KEY: credentials.consumerKey,
-            CONSUMER_SECRET: credentials.consumerSecret,
-            ACCESS_TOKEN: user.userToken,
-            ACCESS_TOKEN_SECRET: user.userTokenSecret,
-        };
-
-        let credentialsString = "";
-        Object.entries(twitterCredentials).forEach(([key, value]) => {
-            credentialsString += `${key}=${value}\n`;
-        });
-
-        clipboardy.writeSync(credentialsString);
-        console.log("Your credentials have been copied to your clipboard!");
-        res.status(200).send(
-            `${JSON.stringify(
-                twitterCredentials
-            )}<script>alert("Your credentials have been copied to your clipboard! You can close this now.");</script>`
-        );
+    let credentialsString = "";
+    Object.entries(twitterCredentials).forEach(([key, value]) => {
+      credentialsString += `${key}=${value}\n`;
     });
+
+    clipboardy.writeSync(credentialsString);
+    console.log("Your credentials have been copied to your clipboard!");
+    res
+      .status(200)
+      .send(
+        `${JSON.stringify(
+          twitterCredentials
+        )}<script>alert("Your credentials have been copied to your clipboard! You can close this now.");</script>`
+      );
+  });
 });
 
 app.listen(port, () => {
-    console.log("Auth generator listening at http://localhost:" + port);
+  console.log("Auth generator listening at http://localhost:" + port);
 });
